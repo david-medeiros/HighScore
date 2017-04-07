4e084abb009c78b1795b1abf377b15c744a6ecb6
DavidMedeiros <davidmedeiros.nave@gmail.com>
2017-04-07 10:47:40 -0300

    HighScore
    (AV3) Highscores
    array[10]; Cada entrada consiste de nome do jogador e sua pontuação; O
    programa deve começar adicionando 10 entradas aleatórias (como se fossem
    os jogadores já lá presentes);
    Ordenar as entradas por pontuação (decrescente); Depois ler entradas do
    usuário (nome + pontuação) e ordenar;
    Sempre 10 entradas (elimina as menores); Criar repositório;
    Extras: o programa deve ler entradas continuamente do usuário;

---
+++ HighScore/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighScore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] name = new string[10] { "david6", "david3", "david5", "david9", "david1", "david0", "david4", "david2", "david7", "david8" };
+            int[] score = new int[10] { 6, 3, 5, 9, 1, 0, 4, 2, 7, 8 };
+
+            for (int d = 0; d < score.Length; d++)
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    if (score[i] > score[i + 1])
+                    {
+                        string guardarname = name[i];
+                        name[i] = name[i + 1];
+                        name[i + 1] = guardarname;
+
+                        int guardarscore = score[i];
+                        score[i] = score[i + 1];
+                        score[i + 1] = guardarscore;
+                    }
+                }
+            }
+
+            foreach (int n in score)
+            {
+                Console.WriteLine(name[n] + " " + score[n]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
